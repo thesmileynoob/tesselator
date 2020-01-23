fdef5433c3e4955f7a4feb0a4a51eb32ef2b684d
reallocz <realloc@protonmail.com>
2020-01-24 00:27:18 +0530

    awesomesauce!

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ object* Player;
 
 object* Ball;
 int Xspeed = 2;
-int Yspeed = 10;
+int Yspeed = 8;
 
 SDL_Texture* Texture;
 Uint8 BgCol[3];  // r,g,b
@@ -102,8 +102,8 @@ int main(int argc, char const* argv[])
         Player->H = 35;
 
         // ball
-        Ball->X = SCR_WIDTH / 2;
-        Ball->Y = 50;
+        Ball->X = Player->X + 40;  // TODO: 40
+        Ball->Y = Player->Y - 50;
         Ball->W = 25;
         Ball->H = 25;
     }
@@ -253,6 +253,30 @@ void update_state(const Uint8* Keys)
     {
         SDL_Rect ball_rect   = RECT(Ball);
         SDL_Rect player_rect = RECT(Player);
+
+        SDL_Rect tile_rect;
+        for (int i = 0; i < TileCount; ++i) {
+            tile* t   = &Tiles[i];
+            tile_rect = RECT(t);
+            if (SDL_HasIntersection(&ball_rect, &tile_rect) == SDL_TRUE) {
+                const int ball_center = ball_rect.x + (ball_rect.w / 2);
+                const int tile_center = tile_rect.x + (tile_rect.w / 2);
+                const int max_xspeed  = 7;
+
+                // -ve means ball to the left
+                const int offset = (ball_center - tile_center) / 2;
+
+                float scale    = 0.2;  // TODO: Better name
+                int new_xspeed = scale * offset;
+                if (new_xspeed > max_xspeed) new_xspeed = max_xspeed;    // clip max
+                if (new_xspeed < -max_xspeed) new_xspeed = -max_xspeed;  // clip max
+
+                // printf("%d -> %d\n", Xspeed, new_xspeed);
+                Xspeed = new_xspeed;  // based on offset
+                Yspeed = -Yspeed;     // just changes direction in Y
+            }
+        }
+
         if (SDL_HasIntersection(&ball_rect, &player_rect) == SDL_TRUE) {
 
             const int ball_center   = ball_rect.x + (ball_rect.w / 2);
