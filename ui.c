d3bde8183a0d9ebaf22f46f9b5bafd8c5aa03dff
reallocz <realloc@protonmail.com>
2020-01-27 00:13:24 +0530

    add ui_fps: display fps!

--- ui.c
+++ ui.c
@@ -9,10 +9,10 @@ extern SDL_Renderer* _renderer;
 extern TTF_Font* UIFont;
 
 /** create a texture and its rect of the score string */
-void ui_score(int score, SDL_Texture** outScoreTexture, SDL_Rect* outScoreRect)
+void ui_score(int score, SDL_Texture** outTexture, SDL_Rect* outRect)
 {
-    static SDL_Texture* score_texture = NULL;
-    static SDL_Rect score_rect;
+    static SDL_Texture* texture = NULL;
+    static SDL_Rect rect;
     static int last_score = -1;  // = -1 so that score_changed is true the first time
 
     int score_changed = last_score != score;
@@ -23,29 +23,70 @@ void ui_score(int score, SDL_Texture** outScoreTexture, SDL_Rect* outScoreRect)
         last_score = score;  // update last score
 
         // cleanup previous texture
-        if (score_texture) SDL_DestroyTexture(score_texture);
+        if (texture) SDL_DestroyTexture(texture);
 
         // construct the string to be displayed
         const int max_len = 15;
-        char score_string[max_len];
-        snprintf(score_string, max_len, "Score: %d", score);
+        char string[max_len];
+        snprintf(string, max_len, "Score: %d", score);
 
         // make a texture of the string
         SDL_Surface* surface =
-            TTF_RenderText_Solid(UIFont, score_string, (SDL_Color){255, 255, 255});
+            TTF_RenderText_Solid(UIFont, string, (SDL_Color){255, 255, 255});
         if (!surface) { printf("UIError: %s\n", TTF_GetError()); }
 
-        score_texture = SDL_CreateTextureFromSurface(_renderer, surface);
-        if (!score_texture) { printf("UIError: %s\n", SDL_GetError()); }
+        texture = SDL_CreateTextureFromSurface(_renderer, surface);
+        if (!texture) { printf("UIError: %s\n", SDL_GetError()); }
 
         SDL_FreeSurface(surface);  // not needed now
 
         // update score_rect
-        score_rect.x = score_rect.y = 0;
-        TTF_SizeText(UIFont, score_string, &score_rect.w, &score_rect.h);
+        rect.x = rect.y = 0;
+        TTF_SizeText(UIFont, string, &rect.w, &rect.h);
     }
-    assert(score_texture);
+    assert(texture);
 
-    *outScoreTexture = score_texture;
-    *outScoreRect    = score_rect;
+    *outTexture = texture;
+    *outRect    = rect;
+}
+
+void ui_fps(float fps, SDL_Texture** outTexture, SDL_Rect* outRect)
+{
+    static SDL_Texture* texture = NULL;
+    static SDL_Rect rect;
+    const int timeout_len = 50;
+    static int timeout    = 0;  // recalculate once in 'timeout_len' iterations
+
+    // re-construct the score_texture iff timeout = 0
+    if (timeout-- <= 0) {
+        timeout = timeout_len;  // reset timeout
+        // printf("UI re-constructing fps texture!\n");
+
+        // cleanup previous texture
+        if (texture) SDL_DestroyTexture(texture);
+
+        // construct the string to be displayed
+        const int max_len = 15;
+        char string[max_len];
+        snprintf(string, max_len, "%.1f FPS", fps);
+
+        // make a texture of the string
+        SDL_Surface* surface =
+            TTF_RenderText_Solid(UIFont, string, (SDL_Color){255, 255, 255});
+        if (!surface) { printf("UIError: %s\n", TTF_GetError()); }
+
+        texture = SDL_CreateTextureFromSurface(_renderer, surface);
+        if (!texture) { printf("UIError: %s\n", SDL_GetError()); }
+
+        SDL_FreeSurface(surface);  // not needed now
+
+        // update score_rect
+        TTF_SizeText(UIFont, string, &rect.w, &rect.h);
+        rect.x = SCR_WIDTH - rect.w - 10;  // 10 padding
+        rect.y = 0;
+    }
+    assert(texture);
+
+    *outTexture = texture;
+    *outRect    = rect;
 }
